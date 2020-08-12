592369d95fec158fd96280af918fd37e16964389
seolranlee <leesugar90@gmail.com>
2020-08-12 15:49:17 +0900

    jqeury passive option추가

--- js/page.js
+++ js/page.js
@@ -106,6 +106,11 @@ var Page = (function() {
   function _init() {
     addEvent();
     getData();
+    jQuery.event.special.touchstart = {
+      setup: function( _, ns, handle ) {
+          this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+      }
+    };
   }
   return {
     init: _init
