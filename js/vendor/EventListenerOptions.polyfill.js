e0fad95f7b6401abda6170da0fcf62fe76d3c1b2
seolranlee <leesugar90@gmail.com>
2020-08-13 10:23:25 +0900

    passive option polyfill

--- js/vendor/EventListenerOptions.polyfill.js
+++ js/vendor/EventListenerOptions.polyfill.js
@@ -1,23 +1,27 @@
 jQuery.event.special.touchstart = {
   setup: function( _, ns, handle ) {
-      this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+      this.addEventListener("touchstart", handle, {passive: true});
+      return false
   }
 };
 
 jQuery.event.special.touchstart = {
   setup: function( _, ns, handle ) {
-      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+      this.addEventListener("touchmove", handle, {passive: true});
+      return false
   }
 };
 
 jQuery.event.special.touchend = {
   setup: function( _, ns, handle ) {
-      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+      this.addEventListener("touchmove", handle, {passive: true});
+      return false
   }
 };
 
 jQuery.event.special.touchend = {
   setup: function( _, ns, handle ) {
-      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+      this.addEventListener("touchmove", handle, {passive: true});
+      return false
   }
 };
